00de79449e63395174ffc028bf4344efa33a9c57
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-29 17:35:42 +0700

    เพิ่มifของn

--- 6320500697-2.c
+++ 6320500697-2.c
@@ -4,23 +4,26 @@ int main ()
     int n,i,max=0;
     scanf ("%d",&n);
     int score[n];
-    for(i=0;i<n;i++)
+    if(n>=2&&n<=1000)
     {
-        scanf ("%d",&score[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-       if(score[i]>=max)
-       {
-           max=score[i];
-       }
-    }
-    for(i=0;i<n;i++)
-    {
-       if(score[i]==max)
-       {
-           printf ("%d ",i+1);
-       }
+        for(i=0; i<n; i++)
+        {
+            scanf ("%d",&score[i]);
+        }
+        for(i=0; i<n; i++)
+        {
+            if(score[i]>=max)
+            {
+                max=score[i];
+            }
+        }
+        for(i=0; i<n; i++)
+        {
+            if(score[i]==max)
+            {
+                printf ("%d ",i+1);
+            }
+        }
     }
     return 0;
 }
