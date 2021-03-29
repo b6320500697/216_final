cb680f54bf64c00b045e55a5fd86075c19e9d171
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-29 19:34:50 +0700

    แก้การแสดงผล

--- 6320500697-2.c
+++ 6320500697-2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-    int n,i,j,k=0,max=0,max2,d=0;
+    int n,i,j,k=0,max=0,max2,min=0,d=0;
     scanf ("%d",&n);
     int score[n];
     if(n>=2&&n<=1000)
@@ -36,10 +36,27 @@ int main ()
             }
             if(k==max)
             {
-               printf ("%d",score[i]);
+                d++;
+                if(d==1)
+                {
+                    max2=score[i];
+                }
+                if(score[i]<max2)
+                {
+                    min=score[i];
+                }
             }
             k=0;
         }
     }
+    if(min>0)
+    {
+        printf ("%d %d ",min,max2);
+    }
+    else
+    {
+        printf ("%d",max2);
+    }
+
     return 0;
 }
